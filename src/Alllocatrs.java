f2fa7a42ccbe61c49a794dae3d51cb85f2d45b0e
Sudarshan <Sudarshan@192.168.0.6>
2017-06-06 12:27:38 +0530

    @aj

--- src/Alllocatrs.java
+++ src/Alllocatrs.java
@@ -6,7 +6,9 @@ public class Alllocatrs {
 	public static String passwrd="pass";
 	public static String login="u_0_q";
 	public void m1(){}
-	public void m2(){}
+	public void m2(){
+	System.out.println("email");
+	}
      			
 
 }
